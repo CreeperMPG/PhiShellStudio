cb7484040711c03b243eda3b4fc5d5e061039b08
CreeperMPG <mpgifn@163.com>
2026-05-29 23:35:24 +0800

    feat: add iOS project scaffolding + CI build workflow

---
+++ PhigrosShellGUI.iOS/Program.cs
@@ -0,0 +1,14 @@
+using UIKit;
+
+namespace PhigrosShellGUI.iOS;
+
+/// <summary>
+/// iOS 应用入口。
+/// </summary>
+public class Program
+{
+    static void Main(string[] args)
+    {
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
